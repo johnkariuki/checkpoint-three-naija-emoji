3cdbf0e29fdf18199189c23a3ea65fcecb5b4c94
John Kariuki <john.kariuki@andela.com>
2016-02-26 11:02:54 +0300

    (chore)[Routes] Reorder routes for logical flow

--- src/routes.php
+++ src/routes.php
@@ -36,107 +36,6 @@ $app->get('/', function (Request $request, Response $response, array $args) {
 });
 
 /**
- * @route GET /emojis
- *
- * @method  emojis (GET) Return all records of emojis from database.
- *
- * @requiredParams none
- * @queryParams none
- *
- * @return JSON     List of all emojis
- */
-$app->get('/emojis', function (Request $request, Response $response, array $args) {
-
-    return EmojiManagerController::getEmojis($request, $response, $args);
-});
-
-/**
- * @route GET /emojis/{id}
- *
- * @method  emojis/{id}(GET, id) Return a record whose primary key matches provided id.
- *
- * @requiredParams id
- * @queryParams id
- *
- * $return JSON     data for a record whose primary key matches provided id.
- */
-$app->get('/emojis/{id}', function (Request $request, Response $response, array $args) {
-
-    return EmojiManagerController::getEmoji($request, $response, $args);
-});
-
-/**
- * Group all standard URI groups that require token authentication.
- *
- * POST /emojis
- * PUT /emojis/{id}
- * PATCH /emojis/{id}
- * DELETE /emojis/{id}
- */
-$app->group('/emojis', function () use ($app) {
-    /**
-     * @route POST /emojis
-     *
-     * @method  /emojis (POST) Add a new emoji record.
-     *
-     * @requiredParams none
-     * @queryParams none
-     *
-     * @return  JSON    data of success or failure in adding new record.
-     */
-    $app->post('', function (Request $request, Response $response, array $args) {
-
-        return EmojiManagerController::postEmoji($request, $response, $args);
-    });
-
-    /**
-     * @route PUT /emojis/{id}
-     *
-     * @method  /emojis/{id} (PUT, id) Update all fields in an emoji record.
-     *
-     * @requiredParams id
-     * @queryParams id
-     *
-     * @return JSON     data of success or failure of put request activity.
-     */
-    $app->put('/{id}', function (Request $request, Response $response, array $args) {
-
-        return EmojiManagerController::putEmoji($request, $response, $args);
-    });
-
-    /**
-     * @route PATCH /emojis/{id}
-     *
-     * @method  /emojis/{id} (PATCH, id) Update specific field in an emoji record.
-     *
-     * @requiredParams id
-     * @queryParams id
-     *
-     * @return JSON     data of success or failure of put request activity.
-     */
-
-    $app->patch('/{id}', function (Request $request, Response $response, array $args) {
-
-        return EmojiManagerController::patchEmoji($request, $response, $args);
-    });
-
-    /**
-     * @route DELETE /emojis/{id}
-     *
-     * @method  /emojis/{id} (DELETE, id) Delete an emoji record.
-     *
-     * @requiredParams id
-     * @queryParams id
-     *
-     * @return Delete an emoji record.
-     */
-    $app->delete('/{id}', function (Request $request, Response $response, array $args) {
-
-        return EmojiManagerController::deleteEmoji($request, $response, $args);
-    });
-})->add($authMiddleware);
-
-/**
  * @route POST /auth/register
  *
  * @method   /auth/register (POST) Register A new user with
@@ -180,4 +79,105 @@ $app->group('/emojis', function () use ($app) {
     $app->get('/auth/logout', function (Request $request, Response $response) {
 
         return AuthController::logoutUser($request, $response);
+    })->add($authMiddleware);
+
+/**
+ * @route GET /emojis
+ *
+ * @method  emojis (GET) Return all records of emojis from database.
+ *
+ * @requiredParams none
+ * @queryParams none
+ *
+ * @return JSON     List of all emojis
+ */
+    $app->get('/emojis', function (Request $request, Response $response, array $args) {
+
+        return EmojiManagerController::getEmojis($request, $response, $args);
+    });
+
+/**
+ * @route GET /emojis/{id}
+ *
+ * @method  emojis/{id}(GET, id) Return a record whose primary key matches provided id.
+ *
+ * @requiredParams id
+ * @queryParams id
+ *
+ * $return JSON     data for a record whose primary key matches provided id.
+ */
+    $app->get('/emojis/{id}', function (Request $request, Response $response, array $args) {
+
+        return EmojiManagerController::getEmoji($request, $response, $args);
+    });
+
+/**
+ * Group all standard URI groups that require token authentication.
+ *
+ * POST /emojis
+ * PUT /emojis/{id}
+ * PATCH /emojis/{id}
+ * DELETE /emojis/{id}
+ */
+    $app->group('/emojis', function () use ($app) {
+        /**
+     * @route POST /emojis
+     *
+     * @method  /emojis (POST) Add a new emoji record.
+     *
+     * @requiredParams none
+     * @queryParams none
+     *
+     * @return  JSON    data of success or failure in adding new record.
+     */
+        $app->post('', function (Request $request, Response $response, array $args) {
+
+            return EmojiManagerController::postEmoji($request, $response, $args);
+        });
+
+    /**
+     * @route PUT /emojis/{id}
+     *
+     * @method  /emojis/{id} (PUT, id) Update all fields in an emoji record.
+     *
+     * @requiredParams id
+     * @queryParams id
+     *
+     * @return JSON     data of success or failure of put request activity.
+     */
+        $app->put('/{id}', function (Request $request, Response $response, array $args) {
+
+            return EmojiManagerController::putEmoji($request, $response, $args);
+        });
+
+    /**
+     * @route PATCH /emojis/{id}
+     *
+     * @method  /emojis/{id} (PATCH, id) Update specific field in an emoji record.
+     *
+     * @requiredParams id
+     * @queryParams id
+     *
+     * @return JSON     data of success or failure of put request activity.
+     */
+
+        $app->patch('/{id}', function (Request $request, Response $response, array $args) {
+
+            return EmojiManagerController::patchEmoji($request, $response, $args);
+        });
+
+    /**
+     * @route DELETE /emojis/{id}
+     *
+     * @method  /emojis/{id} (DELETE, id) Delete an emoji record.
+     *
+     * @requiredParams id
+     * @queryParams id
+     *
+     * @return Delete an emoji record.
+     */
+        $app->delete('/{id}', function (Request $request, Response $response, array $args) {
+
+            return EmojiManagerController::deleteEmoji($request, $response, $args);
+        });
     })->add($authMiddleware);
